3821e9e517ed185f50c894208a08bb6dd3d17641
Robert Talbot <robtalbot89@gmail.com>
2016-05-02 12:12:16 +0100

    basic working force graph

--- Pcon/Api/SkillSearchController.cs
+++ Pcon/Api/SkillSearchController.cs
@@ -15,17 +15,36 @@ namespace Pcon.Api
           
             var graphClient = WebApiConfig.GraphClient;
             var profile = graphClient.Cypher
-                .Match("(user:User)-[:HAS_SKILL]->(skill:Skill), (user)-[:WORKS_IN]->(ou: OU)")
-                .Where((Skill skill) => skill.Name == id)
-                .Return((user, skill, ou)
+                .Match("(user:User)-[:HAS_SKILL]->(qs:Skill), (otherskill:Skill)<-[:HAS_SKILL]-(user)-[:WORKS_IN]->(ou: OU)")
+                .Where((Skill qs) => qs.Name == id)
+                .Return((user, otherskill, ou, qs)
                 => new {
                     User = user.As<User>(),
-                    Skills = skill.CollectAs<User>(),
+                    Skills = otherskill.CollectAs<User>(),
+                    qs =qs.As<User>(),
                     OU = ou.As<OU>()
                 }
                 )
                 .Results;
             return Ok(profile);
+        }
+
+        //MATCH (user:User)-[r:HAS_SKILL]->(skill:Skill) return user, skill
+        public IHttpActionResult Get()
+        {
+            var graphClient = WebApiConfig.GraphClient;
+            var profiles = graphClient.Cypher
+                .Match("(user:User)-[:HAS_SKILL]->(skill:Skill)")
+                .Return((user, skill) => new
+                {
+                    user = user.As<User>(),
+                    skills = skill.CollectAs<User>()
+
+                }
+                )
+                .Results;
+            return Ok(profiles);
+
         }
     }
 }
--- Pcon/Scripts/app/find.js
+++ Pcon/Scripts/app/find.js
@@ -11,6 +11,11 @@
             controller: 'homeController',
             controllerAs: 'profiles'
         })
+        .when('/graph', {
+            templateUrl: '/Scripts/app/partials/graph.html',
+            controller: 'graphController',
+            controllerAs: 'graph'
+        })
       .otherwise({ redirectTo: '/' });
    })
 .controller("homeController", function ($http) {
@@ -30,11 +35,162 @@
     profiles.searchSkills = function () {
         $http.get('/api/skillsearch/', { params: { id: profiles.skill } }).then(
        function (result) {
-           console.log(result);
+
            profiles.searchResults = result.data;
+
+           profiles.searchResults.forEach(function (p) {
+               p.Skills.push(p.qs);
+           });
+
+           console.log(profiles.searchResults);
        },
        function () {
            console.log("failed");
        });
     }
+})
+.controller("graphController", function ($http) {
+    var graph = this;
+    $http.get("/api/skillsearch")
+    .then(
+    function (result) {
+       // console.log(result.data);
+        graph.force = {};
+        graph.force.nodes = [];
+        graph.force.links = [];
+        var index = 0;
+        var nodeTracker = [];
+        result.data.forEach(function (d) {
+            if (nodeTracker.indexOf(d.user.Name) === -1) {
+                nodeTracker.push(d.user.Name);
+            }
+            var userId = nodeTracker.indexOf(d.user.Name);
+            d.skills.forEach(function (s) {
+                if (nodeTracker.indexOf(s.Name) === -1) {
+                    nodeTracker.push(s.Name);
+                }
+
+                var skillId = nodeTracker.indexOf(s.Name);
+                graph.force.links.push({ source: userId, target: skillId });
+
+            });
+
+
+
+            //graph.force.nodes = nodeTracker;
+            nodeTracker.forEach(function (t) {
+                graph.force.nodes.push({ name: t });
+
+            });
+        });
+
+       //console.log(graph.force.nodes);
+       //console.log(graph.force.links);
+
+    },
+    function () {
+        console.log("failed");
+    }
+    );
+})
+.directive("skillgraph", function () {
+    function dlink(scope, element, attrs) {
+        console.log("directive runs");
+       var width = 500,
+       height = 400;
+       var color = d3.scale.category20();
+        var svg = d3.select(element[0]).append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+        var nodes = [];
+        var links = [];
+
+        scope.$watch(function (newVal, oldVal, scope) {
+            console.log(oldVal);
+         
+            if (newVal.links !== undefined && newVal.nodes !== undefined) {
+                nodes = [];
+                links = [];
+                graphStart();
+            }
+        }, true);
+
+        function graphStart() {
+            nodes = scope.nodes;
+            links = scope.links;
+            //console.log(nodes);
+
+            var force = d3.layout.force()
+                .charge(-120)
+                .linkDistance(30)
+                .size([width, height]);
+
+            console.log(links);
+            console.log(nodes);
+            force
+                .nodes(nodes)
+                .links(links)
+                .start();
+            /*
+            var drag = force.drag()
+                .on("dragstart", dragstart);
+            */
+
+            var link = svg.selectAll(".link")
+                .data(links)
+                .enter().append("line")
+                .attr("class", "link")
+                .style("stroke-width", 1);
+
+            var node = svg.selectAll(".node")
+                .data(nodes)
+                .enter().append("circle")
+                .attr("class", "node")
+                .attr("r", 10)
+                /*
+                .style("fill", function (d) {
+                   
+                    return color(d);
+                })
+                */
+                .call(force.drag);
+            /*
+            node.append("title")
+                .text(function (d) { return d; });
+                */
+
+            force.on("tick", function () {
+                // nodes[0].x = width / 2;
+                // nodes[0].y = height / 2;
+                link.attr("x1", function (d) { return d.source.x; })
+                    .attr("y1", function (d) { return d.source.y; })
+                    .attr("x2", function (d) { return d.target.x; })
+                    .attr("y2", function (d) { return d.target.y; });
+
+                node.attr("cx", function (d) { return d.x; })
+                    .attr("cy", function (d) { return d.y; });
+
+            });
+            /*
+            function dragstart(d) {
+                d3.select(this).classed("fixed", d.fixed = true);
+            }
+            */
+        }
+
+
+    }
+
+    return {
+        restrict: 'E',
+        scope: {
+            nodes: '=nodes',
+            links: '=links'
+        },
+        templateUrl: '/scripts/app/partials/force.html',
+        link: dlink
+    };
+
+
 });
