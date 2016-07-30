8eb9bf76dfd5d7b518dc0842e2c291abe4952fc6
rtalbot <robtalbot89@gmail.com>
2016-07-30 09:53:13 +0100

    simplified node tracking

--- Pcon/Scripts/app/find.js
+++ Pcon/Scripts/app/find.js
@@ -110,7 +110,7 @@
             };
         })
     .controller("graphController",
-        function($http) {
+        function($http, arrayFunc) {
             var graph = this;
             graph.filters = [];
             graph.skill = "";
@@ -145,13 +145,13 @@
                             var skillId;
                             var hasId;
                             var ouId;
-                            //if (graph.filters.length === 0) {
                             for (i = 0; i < result.data.length; i++) {
                                 d = result.data[i];
                                 nodeTracker.push({ user: d.user.Name });
                                 userId = nodeTracker.length - 1;
                                 //ous.push(d.ou.Name);
-                                var hasOu = nodeTracker.map(function(e) { return e.ou; }).indexOf(d.ou.Name);
+                                //var hasOu = nodeTracker.map(function (e) { return e.ou; }).indexOf(d.ou.Name);
+                                var hasOu = arrayFunc.hasValue(nodeTracker, d.ou.Name, "ou");
                                 if (hasOu === -1) {
                                     nodeTracker.push({ ou: d.ou.Name });
                                     ouId = nodeTracker.length - 1;
@@ -163,7 +163,8 @@
 
                                 for (var j = 0; j < d.skills.length; j++) {
                                     s = d.skills[j];
-                                    hasId = nodeTracker.map(function(e) { return e.skill; }).indexOf(s.Name);
+                                    //hasId = nodeTracker.map(function (e) { return e.skill; }).indexOf(s.Name);
+                                    hasId = arrayFunc.hasValue(nodeTracker, s.Name, "skill");
                                     if (hasId === -1) {
                                         nodeTracker.push({ skill: s.Name });
                                         skillId = nodeTracker.length - 1;
@@ -173,31 +174,9 @@
                                     graph.force.links.push({ source: userId, target: skillId });
                                 }
                             }
-                            //}
-
-                            // do this if there are filters
-                            //if (graph.filters.length > 0) {
-                            //    for (i = 0; i < result.data.length; i++) {
-                            //        d = result.data[i];
-                            //        nodeTracker.push({ user: d.user.Name });
-                            //        userId = nodeTracker.length - 1;
-                            //        for (var m = 0; m < d.skills.length; m++) {
-                            //            if (graph.filters.indexOf(d.skills[m].Name) !== -1) {
-                            //                s = d.skills[m];
-                            //                hasId = nodeTracker.map(function (e) { return e.skill; }).indexOf(s.Name);
-                            //                if (hasId === -1) {
-                            //                    nodeTracker.push({ skill: s.Name });
-                            //                    skillId = nodeTracker.length - 1;
-                            //                } else {
-                            //                    skillId = hasId;
-                            //                }
-                            //                graph.force.links.push({ source: userId, target: skillId });
-                            //            }
-                            //        }
-                            //    }
-                            //}
-
+                       
                             var nid = 0;
+
                             nodeTracker.forEach(function(t) {
                                 if (t.user !== undefined) {
                                     graph.force.nodes.push({ name: t.user, type: "user", id: nid });
@@ -397,9 +376,9 @@
     .factory("arrayFunc",
         function () {
             return {
-                indexOf : function (myArray, searchTerm, property) {
+                hasValue : function (myArray, searchTerm, property) {
                     for (var i = 0, len = myArray.length; i < len; i++) {
-                        if (myArray[i][property] === searchTerm) return i;
+                        if (myArray[i].hasOwnProperty(property) && myArray[i][property] === searchTerm) return i;
                     }
                     return -1;
                 }
